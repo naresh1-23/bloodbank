7fb6b1e8da2a15a52a6bdb5054995c815ffa51e2
naresh1-23 <nareshtamang20000@gmail.com>
2022-04-30 09:19:52 +0545

    commit

---
+++ home.php
@@ -0,0 +1,169 @@
+<html>
+    <head>
+        <title>Home</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <nav class="navbar">
+            <h2>E bank</h2>
+        </nav>
+        <nav class="sidenavbar">
+            <div class="container">
+                <div class="row sidenav">
+                    <a href="home.php" class = "is-active">Home</a>
+                    <a href="available.php">Looking for blood</a>
+                    <a href="nearbank1.php">Blood bank</a>
+                    <a href="login.php">Log in</a>
+                </div>
+            </div>
+        </nav>
+        <div class="container donate">
+            <h1 class="h1topic">LEARN ABOUT DONATION</h1>
+            <div class="row">
+                <div class="col detail">
+                    <h3 style="color: aliceblue; background-color: red;">Compatible Blood Type Donors</h3>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">Blood Type</th>
+                            <th scope="col">Donate Blood To</th>
+                            <th scope="col">Receive Blood From</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td class="blood">A+</td>
+                            <td>A+ AB+</td>
+                            <td>A+ A- O+ O-</td>
+                        </tr>
+                        <tr>
+                            <td class="blood">O+</td>
+                            <td>O+ A+ B+ AB+</td>
+                            <td>O+ O-</td>
+                        </tr>
+                        <tr>
+                            <td class="blood">B+</td>
+                            <td>B+ AB+</td>
+                            <td>B+ B- O+ O-</td>
+                        </tr>
+                        <tr>
+                            <td class="blood">AB+</td>
+                            <td>AB+</td>
+                            <td>Everyone</td>
+                        </tr>
+                        <tr>
+                            <td class="blood">A-</td>
+                            <td>A+ A- AB- AB+</td>
+                            <td>A- O-</td>
+                        </tr>
+                        <tr>
+                            <td class="blood">O-</td>
+                            <td>Everyone</td>
+                            <td>O-</td>
+                        </tr>
+                        <tr>
+                            <td class="blood">B-</td>
+                            <td>B+ B- AB+ AB-</td>
+                            <td>B- O-</td>
+                        </tr>
+                        <tr>
+                            <td class="blood">AB-</td>
+                            <td>AB+ AB-</td>
+                            <td>AB- A- B- O-</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <hr style="margin-top:50px; margin-bottom:50px; border:bold;"> 
+        <div class="container hospital" id="hospital">
+            <h2 class="hea" id="heas">Blood Bank</h2><br><br>
+            <table class="table">
+                <thead>
+                <tr style="background-color: rgb(245, 96, 96);">
+                    <th scope="col">S.No</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Phone</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="content">
+                    <th scope="row">1</th>
+                    <td><a href="available.html">Nepal Red Cross Society Blood Bank, Soalteemod</a></td>
+                    <td>Kathmandu</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">2</th>
+                    <td><a href="available.html">Tribhuwan University Teaching Hospital Blood Center</a></td>
+                    <td>Kathmandu</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">3</th>
+                    <td><a href="available.html">TU Lions Blood Transfusion & Research Center (#TULBTRC)</a></td>
+                    <td>Kirtipur</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">4</th>
+                    <td><a href="available.html">Nepal Red Cross Society Blood Bank, Soalteemod</a></td>
+                    <td>Kathmandu</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">5</th>
+                    <td><a href="available.html">Tribhuwan University Teaching Hospital Blood Center</a></td>
+                    <td>Kathmandu</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">6</th>
+                    <td><a href="available.html">TU Lions Blood Transfusion & Research Center (#TULBTRC)</a></td>
+                    <td>Kirtipur</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">7</th>
+                    <td><a href="available.html">Nepal Red Cross Society Blood Bank, Soalteemod</a></td>
+                    <td>Kathmandu</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">8</th>
+                    <td><a href="available.html">Tribhuwan University Teaching Hospital Blood Center</a></td>
+                    <td>Kathmandu</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">9</th>
+                    <td><a href="available.html">TU Lions Blood Transfusion & Research Center (#TULBTRC)</a></td>
+                    <td>Kirtipur</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">10</th>
+                    <td><a href="available.html">Nepal Red Cross Society Blood Bank, Soalteemod</a></td>
+                    <td>Kathmandu</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">11</th>
+                    <td><a href="available.html">Tribhuwan University Teaching Hospital Blood Center</a></td>
+                    <td>Kathmandu</td>
+                    <td>01-5006465</td>
+                </tr>
+                <tr class="content">
+                    <th scope="row">12</th>
+                    <td><a href="available.html">TU Lions Blood Transfusion & Research Center (#TULBTRC)</a></td>
+                    <td>Kirtipur</td>
+                    <td>01-5006465</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
